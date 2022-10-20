19bdad3caaa88a05e0816822c1a9178be9d93449
Paulo Rogerio <paulo@intranet.com>
2022-10-20 13:41:58 -0300

    Adcionando middleware

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,14 @@
+import { NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log({
+      headers: req.headers,
+      body: req.body,
+      resp: res,
+    });
+
+    next();
+  }
+}
